6b46b77c0c74802c6e7632f27444630d415ea4d0
hendridg <hendridg@gmail.com>
2021-12-13 17:42:07 -0400

    Add index.js file with query elements

---
+++ index.js
@@ -0,0 +1,6 @@
+var menuBar = document.querySelector('.menu-bar');
+var btnClose = document.querySelector('.btn-close');
+
+btnClose.addEventListener('click', () => {
+  console.log('click button close');
+});
