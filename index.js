75beb7433df22b071736a2b9021c06d5998af026
hendridg <hendridg@gmail.com>
2021-12-15 13:20:55 -0400

    Add function to handle events in botton to popup

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const menuBar = document.querySelector(".menu-bar"); // mobile menu container
-const btnClose = document.querySelector(".btn-close"); // button for mobile menu
-const btnMenu = document.querySelector(".btn-menu"); // button for the desktop menu
-const navLink = document.querySelectorAll("#mobile-menu-bar li .nav-link");
-const tog = document.getElementsByClassName("toggle");
+const menuBar = document.querySelector('.menu-bar'); // mobile menu container
+const btnClose = document.querySelector('.btn-close'); // button for mobile menu
+const btnMenu = document.querySelector('.btn-menu'); // button for the desktop menu
+const navLink = document.querySelectorAll('#mobile-menu-bar li .nav-link');
+const tog = document.getElementsByClassName('toggle');
 const togg = Array.from(tog); // convert the nodelist to an array
-const popupMenu = document.createElement("div");
+let popupMenu = document.createElement('div');
 
-btnClose.addEventListener("click", () => {
-  if (menuBar.style.display === "flex") {
-    menuBar.style.display = "none";
+btnClose.addEventListener('click', () => {
+  if (menuBar.style.display === 'flex') {
+    menuBar.style.display = 'none';
   }
 });
 
-btnMenu.addEventListener("click", () => {
-  if (menuBar.style.display === "none") {
-    menuBar.style.display = "flex";
+btnMenu.addEventListener('click', () => {
+  if (menuBar.style.display === 'none') {
+    menuBar.style.display = 'flex';
   } else {
-    menuBar.style.display = "none";
+    menuBar.style.display = 'none';
   }
 });
 
 navLink.forEach((link) => {
-  link.addEventListener("click", () => {
-    if (menuBar.style.display === "flex") {
-      menuBar.style.display = "none";
+  link.addEventListener('click', () => {
+    if (menuBar.style.display === 'flex') {
+      menuBar.style.display = 'none';
     }
   });
 });
@@ -45,69 +45,69 @@ navLink.forEach((link) => {
 
 const projects = [
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App press',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
   {
-    projectImage: "images/work-screenshots/Bankist.PNG",
-    projectName: "Bankist App",
-    technologies: ["javaScript", "Css", "Html"],
+    projectImage: 'images/work-screenshots/Bankist.PNG',
+    projectName: 'Bankist App',
+    technologies: ['javaScript', 'Css', 'Html'],
     description:
-      "A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques",
+      'A beautiful website with a lot of different components and visual effects that require some pretty advanced DOM manipulation techniques',
     links: [
-      "https://bondok6.github.io/Bankist/",
-      "https://github.com/Bondok6/Bankist",
+      'https://bondok6.github.io/Bankist/',
+      'https://github.com/Bondok6/Bankist',
     ],
   },
 ];
@@ -117,14 +117,14 @@ function callPop(index) {
   <div class="overlay"></div>
   <div class="content">
     <div class="close-btn">&times;</div>
-    <img src="./images/bg.svg" alt="" />
+    <img src="./images/bg.svg" alt="close-btn" />
     <div class="pop-container">
       <div class="pop-item">
         <h2>${projects[index].projectName}</h2>
         <ul>
           <li>${projects[index].technologies[0]}</li>
           <li>${projects[index].technologies[1]}</li>
-          <li>${projects[index].technologies[0]}</li>
+          <li>${projects[index].technologies[2]}</li>
         </ul>
         <p>
         ${projects[index].description}
@@ -147,9 +147,11 @@ function callPop(index) {
 // popup logic
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < togg.length; i++) {
-  togg[i].addEventListener("click", async () => {
-    popupMenu.innerHTML = await callPop(i);
-    const toggle = document.getElementById("popup-1");
-    toggle.classList.toggle("active");
+  togg[i].addEventListener('click', () => {
+    popupMenu.innerHTML = callPop(i);
+    document.body.appendChild(popupMenu);
+    const toggle = document.getElementById('popup-1');
+    console.log(toggle);
+    toggle.classList.toggle('active');
   });
 }
